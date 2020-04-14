62e0cec784bfdce86f7ad2deee59a16785bbae98
adjeng <sekariniadjeng@gmail.com>
2020-04-14 22:47:33 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class OperatorBinary {
+    
+    // operator (=) sebagai assignment/penugasan
+    // menempatkan nilai kedalam variable
+    // baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = "Adjeng Sekarrini";
+    int jumlahSKS = 24;
+    double indeksPresentasiKumulatif = 3.89;
+
+}
