37c5256679711f1a77665ed9f703dc9351e9db25
adjeng <sekariniadjeng@gmail.com>
2020-04-14 23:29:16 +0800

    Brancing ifelse Conditon1 false and Condition2 true

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -2,13 +2,16 @@ package percabangan;
 
 public class IfElse {
     public static void main(String[] args) {
-        double totalBelanja = 25000;
-        double uangDiDompet = 50000;
+        double totalBelanja = 25000; // di file .ppt slide 30 ini sebenarnya 53000
+        double uangDiDompet = 50000; // dan ini 25000 karena disini disuruh Condition2 bernilai True sehingga Statment2 jalan
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, cukup jajan anda");
+        }else if (uangDiDompet>totalBelanja){
+            double angsul = uangDiDompet-totalBelanja;
+            System.out.println("Uang cukup, angsul : "+angsul);
         }else{
-            System.out.println("Uang cukup, berelaan lah");
+            System.out.println("Uang pas.. Uwuuu...");
         }
     }
 }
